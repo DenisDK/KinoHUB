d6c3f618e617e05d1ad6742546dcf7772c82bdac
DenisDK <unworkdk@gmail.com>
2024-05-17 17:07:34 +0300

    Change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    //  domains: ["lh3.googleusercontent.com"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "image.tmdb.org",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
         pathname: "**",
       },
     ],
